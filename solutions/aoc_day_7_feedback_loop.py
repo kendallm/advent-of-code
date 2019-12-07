1be7af58eec3218e63755eaf59a1d3801ae6c5bd
kendallm <1841529+kendallm@users.noreply.github.com>
2019-12-07 14:00:31 -0800

    Refactor day  7

--- solutions/aoc_day_7_feedback_loop.py
+++ solutions/aoc_day_7_feedback_loop.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     sum = 0
     with open("../input/input_7.txt") as f:
         memory = f.read()
-    # memory = '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'
     memory = memory.split(',')
     amps = [Computer(memory.copy()),
         Computer(memory.copy()),
