b1504ca2d278f2b6e3e0589513ac082524f0b9f3
kendallm <1841529+kendallm@users.noreply.github.com>
2019-12-24 14:29:42 -0800

    Finish day 24 part 1

---
+++ solutions/aoc_day_14.py
@@ -0,0 +1,47 @@
+from collections import *
+from math import *
+
+def main():
+    graph = set()
+    fuel = None
+    with open("../input/input_14.txt") as f:
+        for line in f:
+            in_out = line.strip().split(' => ')
+            output_chemical = in_out[-1].split(" ")
+            input_chemicals = in_out[0].split(", ")
+            input_chemicals = [tuple(x.split(" ")) for x in input_chemicals]
+            output_chemical = tuple(output_chemical)
+            
+            prev = Node(output_chemical[1])
+            graph.add(prev)
+            for cost in input_chemicals:
+                if prev.name == "FUEL":
+                    fuel = prev
+                node = Node(cost[1])
+                prev.edges.add((node, cost[0]))
+                graph.add(node)
+            prev = node
+    
+    
+    q = list(fuel.edges)
+    cost = []
+    while len(q) > 0:
+        print("once")
+        node = q.pop(0)
+        cost.append(node[1])
+        # if node[0].name == "ORE":
+        #     print("FOUND")
+        #     break
+        # else:
+        q = q + list(node[0].edges)
+        
+    print(cost)
+# def dp(chemical, reactions):
+#     if chemical = "FUEL":
+#         return 0
+#     for reaction in reactions:
+#         if reaction[1] == "ORE":
+#             return reaction[0] + dp 
+    
+if __name__ == "__main__":
+    main()
