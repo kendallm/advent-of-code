cdb6b23fc1792d832b2287dc0c862d6f9b363079
Kendall Morgan <1841529+kendallm@users.noreply.github.com>
2021-12-01 22:22:34 -0800

    Refactor get_inputs

--- utils/get_inputs.py
+++ utils/get_inputs.py
@@ -1,16 +1,21 @@
+import coloredlogs, logging
 import sys
 import requests
 from pathlib import Path
 
+from requests.models import Response
+
+
 class ProblemParser:
     def __init__(self):
         pass
-    
+
     def load_input(self, year, day):
         with open(f"{year}/input/input_{day}.txt") as f:
             lines = f.readlines()
         return lines
-    
+
+
 PYTHON_SOLUTION_TEMPLATE = """import sys
 from pathlib import Path
 path_root = Path(__file__).parents[2]
@@ -22,31 +27,57 @@ from utils.get_inputs import ProblemParser
 lines = ProblemParser().load_input(%s, %s)
 """
 
-if __name__ == '__main__':
-    s = requests.session()
-    year = sys.argv[1]
-    problem_number = sys.argv[2]
-    
-    p = Path(f'{year}/input/input_{problem_number}.txt')
-    if p.is_file():
-        print("Input file already exists")
-        exit(1)
+LOGGER = logging.getLogger()
+coloredlogs.install(level='DEBUG')
 
-    with open('.session') as f:
-        session_cookie = f.read().strip() 
-    cookie_obj = requests.cookies.create_cookie(domain='.adventofcode.com',name='session',value=session_cookie)
+
+def get_session_cookie() -> str:
+    with open(".session") as f:
+        session_cookie = f.read().strip()
+    return session_cookie
+
+
+def download_input(s, year, problem_number, session_cookie) -> Response:
+    cookie_obj = requests.cookies.create_cookie(
+        domain=".adventofcode.com", name="session", value=session_cookie
+    )
     s.cookies.set_cookie(cookie_obj)
 
     url = f"https://adventofcode.com/{year}/day/{problem_number}/input"
     response = s.get(url)
+    return response
 
+
+def generate_python_template(year, problem_number):
+    solution = Path(f"{year}/solutions/day{problem_number}.py")
+    if solution.is_file():
+        LOGGER.warning("Python solution file already exists")
+        return
+    solution.touch()
+    with open(f"{year}/solutions/day{problem_number}.py", "w") as f:
+        f.write(PYTHON_SOLUTION_TEMPLATE % (year, problem_number))
+    LOGGER.info("Created python solution file")
+
+
+def download_input_file(year, problem_number):
+    s = requests.session()
+    p = Path(f"{year}/input/input_{problem_number}.txt")
+    if p.is_file() and p.stat().st_size > 0:
+        LOGGER.warning("Input file already exists")
+        return
+    session_cookie = get_session_cookie()
+    response = download_input(s, year, problem_number, session_cookie)
     if response.ok:
-        print(url)
-        with open(f'{year}/input/input_{problem_number}.txt', 'w') as f:
-            f.write(response.content.decode('utf-8'))
-        Path(f'{year}/solutions/day{problem_number}.py').touch()
-        with open(f'{year}/solutions/day{problem_number}.py', 'w') as f:
-            f.write(PYTHON_SOLUTION_TEMPLATE % year, problem_number)
+        with open(f"{year}/input/input_{problem_number}.txt", "w") as f:
+            f.write(response.content.decode("utf-8"))
     else:
-        print("Input file not found/available")
+        LOGGER.error("Input file not found/available")
         sys.exit(1)
+    LOGGER.info("Downloaded input file")
+
+if __name__ == "__main__":
+    year = sys.argv[1]
+    problem_number = sys.argv[2]
+
+    download_input_file(year, problem_number)
+    generate_python_template(year, problem_number)
