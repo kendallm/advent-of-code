5900b23f93a20829ec56f73a72ce50c242d883d4
Kendall Morgan <1841529+kendallm@users.noreply.github.com>
2021-12-01 21:51:23 -0800

    Cache input file

--- utils/get_inputs.py
+++ utils/get_inputs.py
@@ -11,10 +11,27 @@ class ProblemParser:
             lines = f.readlines()
         return lines
     
+PYTHON_SOLUTION_TEMPLATE = """import sys
+from pathlib import Path
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
+from utils.get_inputs import ProblemParser
+
+
+lines = ProblemParser().load_input(%s, %s)
+"""
+
 if __name__ == '__main__':
     s = requests.session()
     year = sys.argv[1]
     problem_number = sys.argv[2]
+    
+    p = Path(f'{year}/input/input_{problem_number}.txt')
+    if p.is_file():
+        print("Input file already exists")
+        exit(1)
+
     with open('.session') as f:
         session_cookie = f.read().strip() 
     cookie_obj = requests.cookies.create_cookie(domain='.adventofcode.com',name='session',value=session_cookie)
@@ -29,16 +46,7 @@ if __name__ == '__main__':
             f.write(response.content.decode('utf-8'))
         Path(f'{year}/solutions/day{problem_number}.py').touch()
         with open(f'{year}/solutions/day{problem_number}.py', 'w') as f:
-            f.write(f"""import sys
-from pathlib import Path
-path_root = Path(__file__).parents[2]
-sys.path.append(str(path_root))
-
-from utils.get_inputs import ProblemParser
-
-
-lines = ProblemParser().load_input({year}, {problem_number})
-""")
+            f.write(PYTHON_SOLUTION_TEMPLATE % year, problem_number)
     else:
         print("Input file not found/available")
         sys.exit(1)
