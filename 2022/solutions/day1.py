f1215bbce2f69ef6f716e09f20c95fe0010cb882
Kendall Morgan <1841529+kendallm@users.noreply.github.com>
2022-11-30 21:31:25 -0800

    Finish day 1

---
+++ 2022/solutions/day1.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
+from utils.get_inputs import ProblemParser
+
+
+lines = ProblemParser().load_input(2022, 1)
+
+count = 0
+calories = []
+
+for line in lines:
+    if line == "":
+        calories.append(count)
+        count = 0
+        continue
+    c = int(line)
+    count = count + c
+
+calories.sort(reverse=True)
+
+print(f"Part 1: {calories[0]}")
+print(f"Part 2: {calories[0] + calories[1] + calories[2]}")
