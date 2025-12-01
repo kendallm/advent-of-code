cbd5fa2c3c710d4305390a602490a028a8954bb2
Kendall Morgan <1841529+kendallm@users.noreply.github.com>
2025-12-01 11:08:24 -0500

    feat: add uv

---
+++ 2025/day1.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+
+from utils.get_inputs import ProblemParser
+
+
+def main():
+    lines = ProblemParser().load_input(2025, 1)
+
+if __name__ == '__main__':
+    main()
