634c18b7860ed1cc78ea5fff4af8b0bf98f7b6e2
Kendall Morgan <1841529+kendallm@users.noreply.github.com>
2025-12-03 12:48:55 -0500

    feat: 2025 day 1

--- 2025/day1.py
+++ 2025/day1.py
@@ -9,6 +9,29 @@ from utils.get_inputs import ProblemParser
 
 def main():
     lines = ProblemParser().load_input(2025, 1)
+    curr = 50
+    counts = 0
+    count_any = 0
+    for line in lines:
+        scalar = 1
+        if line[0] == 'L':
+            scalar = -1
+        abs_rot = abs(int(line[1:]) )
+        rot = abs_rot* scalar
+        count_any += abs_rot // 100
+        div = (abs_rot % 100) * scalar
+        extra = div + curr
+        if curr != 0 and extra < 1 or extra >= 100:
+            count_any += 1
+        curr = (curr + rot) % 100
+        if curr == 0:
+            counts += 1
+    print(counts)
+    print(count_any)
+
+    curr = 50
 
 if __name__ == '__main__':
     main()
+
+# curr=84, rot=-212, times=3
